84431b69a29998035e0dcacce9038fc7e4122ded
EmanuelVolpe <42517375+EmanuelVolpe@users.noreply.github.com>
2022-12-21 11:33:14 -0300

    Agrega Ejercicio1

---
+++ src/codigo/Main.java
@@ -0,0 +1,19 @@
+package codigo;
+
+public class Main {
+    public static void main(String[] args) {
+        //Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
+
+        int num1 = 12;
+        long num2 = 1222223;
+        double num3 = 12.3;
+        boolean isTrue = true;
+        String nombre = "Emanuel";
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(isTrue);
+        System.out.println(nombre);
+    }
+}
