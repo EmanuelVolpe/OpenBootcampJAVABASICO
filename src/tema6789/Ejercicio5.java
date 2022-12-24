8a87e9829c1b7885bb677bdec393a95e708e4010
EmanuelVolpe <42517375+EmanuelVolpe@users.noreply.github.com>
2022-12-24 11:43:07 -0300

    Agrega Ejercicio5

---
+++ src/tema6789/Ejercicio5.java
@@ -0,0 +1,25 @@
+package tema6789;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        //Crea un ArrayList de tipo String, con 4 elementos.
+        //Cópialo en una LinkedList.
+        //Recorre ambos mostrando únicamente el valor de cada elemento.
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Manu");
+        list.add("Tota");
+        list.add("Ana");
+        list.add("Joaco");
+        System.out.println(list);
+        
+        System.out.println("------------------------------------------");
+
+        LinkedList<String> links = new LinkedList<String>();
+        links.addAll(list);
+        System.out.println(links);
+    }
+}
