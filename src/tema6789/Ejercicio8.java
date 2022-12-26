ca03ae654a9f3008db235d1b72c434d03df5fd5e
EmanuelVolpe <42517375+EmanuelVolpe@users.noreply.github.com>
2022-12-26 08:33:46 -0300

    Arregla 8

--- src/tema6789/Ejercicio8.java
+++ src/tema6789/Ejercicio8.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Array;
 
 public class Ejercicio8 {
     public static void main(String[] args) {
@@ -11,6 +12,8 @@ public class Ejercicio8 {
         //La tarea de la función será realizar la copia del fichero dado en el parámetro "fileIn" al fichero dado en "fileOut".
         leerTodo();
     }
+
+
     public static void leerTodo() {
         try {
             InputStream fichero = new FileInputStream("/etc/passwd");
