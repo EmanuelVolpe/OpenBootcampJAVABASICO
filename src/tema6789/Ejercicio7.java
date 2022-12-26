84a554f6a465e3b8385e8e4b0c326faf32756e6e
EmanuelVolpe <42517375+EmanuelVolpe@users.noreply.github.com>
2022-12-26 07:49:46 -0300

    Agrega Ejercicio 7

---
+++ src/tema6789/Ejercicio7.java
@@ -0,0 +1,32 @@
+package tema6789;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        //Crea una función DividePorCero. Esta, debe generar una excepción ("throws") a su llamante del tipo
+        //ArithmeticException que será capturada por su llamante (desde "main", por ejemplo).
+        //Si se dispara la excepción, mostraremos el mensaje "Esto no puede hacerse".
+        //Finalmente, mostraremos en cualquier caso: "Demo de código".
+        errorDivisionPorCeroTry();
+    }
+
+    public static void errorDivisionPorCeroTry() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce dos números: ");
+        int numeroA = scanner.nextInt();
+        int numeroB = scanner.nextInt();
+
+        try {
+            int resultado = numeroA / numeroB;
+            System.out.println("El resultado es: " + resultado);
+        } catch (ArithmeticException var4) {
+            System.out.println(var4);
+            System.out.println("Esto no puede hacerse");
+        } finally {
+            System.out.println("Demo de código");
+        }
+
+    }
+
+}
